d94f31f903b1c0c19362538afa9125e67130bcf3
Roman <romon2002@gmail.com>
2021-08-01 23:01:51 +0300

    isFunction support several arguments

--- test/lib/is-function.spec.js
+++ test/lib/is-function.spec.js
@@ -4,13 +4,23 @@ describe('Test isFunction function', () => {
   test('isFunction is defined.', () => expect(isFunction).toBeDefined());
   test('isFunc is defined.', () => expect(isFunc).toBeDefined());
 
-  test('isFunction returns true if object passed', () => {
-    expect(isFunction(() => {})).toBe(true);
+  [[() => {}], [() => {}, () => {}]].forEach((val) => {
+    test(`isFunction returns true if ${val.length} function(s) passed as arguments`, () => {
+      expect(isFunction(...val)).toBe(true);
+    });
   });
 
-  ['string', null, 5, {}].forEach((val) => {
+  test('isFunction returns false if nothing passed', () => {
+    expect(isFunction()).toBe(false);
+  });
+
+  ['string', null, 5, undefined, {}].forEach((val) => {
     test(`isFunction returns false if ${val} passed`, () => {
       expect(isFunction(val)).toBe(false);
     });
+  });
+
+  test('isFunction returns false if several functions and string passed', () => {
+    expect(isFunction(() => {}, () => {}, 'string')).toBe(false);
   });
 });
